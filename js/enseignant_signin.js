06329df1625d90ab7d425b25e5f7b9f602a6d835
LahmarTabai <79151540+LahmarTabai@users.noreply.github.com>
2021-11-08 17:35:23 +0100

    modification

--- js/enseignant_signin.js
+++ js/enseignant_signin.js
@@ -11,3 +11,25 @@ function toggle() {
         state = true;
     }
 };
+
+
+/***********************************/
+
+// xmlHTTPRequest :
+
+function fullData() {
+    document.getElementById('but').disabled=true; 
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://itic-pap.rihan.fr/auth/login");
+    request.onreadystatechange = function() {
+        console.log('state change', this);
+        if(this.readyState === 4 && this.status === 200) {
+            document.getElementById('result').innerHTML = this.responseText;
+        }else {
+            document.getElementById('result').innerHTML = "Forbidden";
+        }
+    };
+    var myForm = document.getElementById('myForm');
+    var formData = new FormData(myForm);
+    request.send(formData);
+};
